72c10074c81a2d46faf96f172df16bfbe4d34100
tybrianna <tybriannahall@gmail.com>
2026-05-14 18:44:04 -0400

    added favorites context file within context folder and included codes inside to be able to have a favorites section and edit it

---
+++ src/context/FavoritesContext.tsx
@@ -0,0 +1,65 @@
+import {
+  createContext,
+  useContext,
+  ReactNode,
+} from "react";
+
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+type FavoritesContextType = {
+  favorites: string[];
+  addFavorite: (id: string) => void;
+  removeFavorite: (id: string) => void;
+  isFavorite: (id: string) => boolean;
+};
+
+const FavoritesContext =
+  createContext<FavoritesContextType | null>(null);
+
+export const FavoritesProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [favorites, setFavorites] =
+    useLocalStorage<string[]>("favorites", []);
+
+  const addFavorite = (id: string) => {
+    setFavorites([...favorites, id]);
+  };
+
+  const removeFavorite = (id: string) => {
+    setFavorites(
+      favorites.filter((fav) => fav !== id)
+    );
+  };
+
+  const isFavorite = (id: string) => {
+    return favorites.includes(id);
+  };
+
+  return (
+    <FavoritesContext.Provider
+      value={{
+        favorites,
+        addFavorite,
+        removeFavorite,
+        isFavorite,
+      }}
+    >
+      {children}
+    </FavoritesContext.Provider>
+  );
+};
+
+export const useFavorites = () => {
+  const context = useContext(FavoritesContext);
+
+  if (!context) {
+    throw new Error(
+      "useFavorites must be used within FavoritesProvider"
+    );
+  }
+
+  return context;
+};
